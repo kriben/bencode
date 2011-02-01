7f668b5dae8442555dcdd9443d6cba51c4bea903
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2011-02-01 22:05:43 +0100

    Simplify example code slightly.

--- examples/printvalues.cpp
+++ examples/printvalues.cpp
@@ -18,30 +18,27 @@ int main(int argc, char* argv[])
     return -1;
   }
 
-
   std::string filename = argv[1];
-  std::cout << "Reading " << filename << "\n";
-
   std::ifstream file(filename.c_str());
-  if (file) {
-    std::stringstream buffer;
-    buffer << file.rdbuf();
-    file.close();
-
-    try {
-      Value torrent =
-	boost::get<ValueDictionary>(BencodeDecoder::decode(buffer.str()));
-      boost::apply_visitor(PrettyPrinter(), torrent);
-    }
-    catch (std::invalid_argument& e) {
-      std::cerr << "An error occurred: " << e.what() << std::endl;
-      return -1;
-    }
-
-    return 0;
-  }
-  else {
-    std::cerr << "Unable to open file.\n";
+  if (!file) {
+    std::cerr << "Unable to open file: " << filename << "\n";
     return -1;
   }
+
+  std::cout << "Reading " << filename << "\n";
+  std::stringstream buffer;
+  buffer << file.rdbuf();
+  file.close();
+
+  try {
+    Value torrent =
+      boost::get<ValueDictionary>(BencodeDecoder::decode(buffer.str()));
+    boost::apply_visitor(PrettyPrinter(), torrent);
+  }
+  catch (std::invalid_argument& e) {
+    std::cerr << "An error occurred: " << e.what() << "\n";
+    return -1;
+  }
+
+  return 0;
 }
